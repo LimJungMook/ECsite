37df832e3abcb38910f80c20a9a59a1923d2a854
mook <jerry970118@naver.com>
2024-10-26 22:59:26 +0900

    productList 화면, 화면 이동

--- src/main/java/com/mookShopping/mook/controller/ProductController.java
+++ src/main/java/com/mookShopping/mook/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.mookShopping.mook.controller;
 
 import com.mookShopping.mook.domain.Product;
+import com.mookShopping.mook.service.ProductService;
 import com.mookShopping.mook.web.ProductForm;
 import jakarta.validation.Valid;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,9 +13,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @GetMapping("/product/new")
     public String newProductForm(Model model) {
@@ -21,8 +28,32 @@ public class ProductController {
         return "productRegi";
     }
 
+    @PostMapping("/product/new")
+    public String saveProduct(@Valid ProductForm productForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "productRegi";
+        }
+
+        Product product = new Product();
+        product.setName(productForm.getName());
+        product.setQuantity(productForm.getQuantity());
+        product.setPrice(productForm.getPrice());
+        Long saveProduct = productService.saveProduct(product);
+        log.info("saveProduct: " + saveProduct);
+        return "redirect:/product/productList";
+    }
+
+    @GetMapping("/product/productList")
+    public String getList(Model model) {
+        List<Product> productList = productService.findAllProduct();
+        model.addAttribute("product",productList );
+        return "productList";
+    }
+
+
     @GetMapping("/product/detail")
     public String getDetail(Model model) {
+
         model.addAttribute("product", new ProductForm());
         return "detail";
     }
@@ -32,12 +63,5 @@ public class ProductController {
         return "productModify";
     }
 
-    @PostMapping("/product/new")
-    public String saveProduct(@Valid ProductForm productForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "productRegi";
-        }
 
-        return "main";
-    }
 }
